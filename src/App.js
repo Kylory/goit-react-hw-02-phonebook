65f1662e8234394d7f0963bb95a1b87388c64a4b
Kylory <72569852+Kylory@users.noreply.github.com>
2021-07-16 22:16:54 +0300

    added all

--- src/App.js
+++ src/App.js
@@ -1,34 +1,64 @@
 import React, { Component } from 'react';
-import Form from './components/Form/Form';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactsList from './components/ContactsList/ContactsList';
+import SearchContacts from './components/SearchContacts/SearchContacts';
 import shortid from 'shortid';
-import Contacts from './components/Contacts/Contacts'
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
-    name: '',
-    number: '',
   };
 
   formSubmitHandler = data => {
     const { name, number } = data;
-    const {contacts} = this.state
-    this.setState({ name: name, number: number });
-    contacts.push({ id:shortid.generate(), name: name, number: number })
-  }
+    const { contacts } = this.state;
 
-  handleSubmit = e => {
-    e.preventDefault();
+    this.setState({ name: name, number: number });
+    contacts.push({ id: shortid.generate(), name: name, number: number });
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
   };
 
   render() {
     return (
       <>
-        <Form onSubmit={this.formSubmitHandler} />
-        <Contacts
-          title='Contacts'
-          contacts={this.state.contacts} />
+        <h2>Phonebook</h2>
+        <ContactForm
+          onSubmit={this.formSubmitHandler}
+          contacts={this.state.contacts}
+        />
+        <h2>Contacts</h2>
+        <SearchContacts
+          value={this.state.filter}
+          onChange={this.changeFilter}
+        />
+        <ContactsList
+          contacts={this.getFilteredContacts()}
+          onDelete={this.deleteContact}
+        />
       </>
     );
   }
